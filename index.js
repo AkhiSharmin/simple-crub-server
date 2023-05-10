4c8887c05dcbc55b3e9f2d728385bf3c88e8bc6f
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-11 03:28:46 +0600

    added mongodb

--- index.js
+++ index.js
@@ -27,7 +27,28 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
+        const database = client.db("usersDB");
+        const userCollection = database.collection("user");
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            console.log('new user', user);
+            const result = await userCollection.insertOne(doc);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
