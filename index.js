32f7ad71cf0014120bd914535626a2bb9e8fc4f2
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-12 05:07:14 +0600

    updated user

--- index.js
+++ index.js
@@ -61,6 +61,22 @@ async function run() {
         })
 
 
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            console.log(id, user);
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatedUser = {
+                $set: {
+                    name: user.name,
+                    email: user.email
+                }
+            }
+        })
+
+
+
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             console.log('please delete from Database', id);
