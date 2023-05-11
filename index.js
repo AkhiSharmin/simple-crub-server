824eb79baba00d827e9a034d29eae834c28c1203
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-12 02:42:01 +0600

    added async

--- index.js
+++ index.js
@@ -52,9 +52,12 @@ async function run() {
         })
 
 
-        app.delete('/user/:id', (req, res) => {
+        app.delete('/user/:id', async (req, res) => {
             const id = req.params.id;
             console.log('please delete from Database', id);
+            const query = { _id: id }
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
         })
 
 
